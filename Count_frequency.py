ba3b04da1f9db30bce7b05f1c2593f73fec0827b
charul97 <charulagrawal97@gmail.com>
2020-10-02 14:02:25 +0530

    Added lines 1,2,11
    Explained code and added parenthesis in print statement

--- Count_frequency.py
+++ Count_frequency.py
@@ -1,3 +1,5 @@
+# it returns a dictionary data structure whose keys are array elements and values are their 
+# corresponding frequencies {1: 4, 2: 4, 3: 2, 5: 2, 4: 1} 
 import collections 
 
 def CountFrequency(arr): 
@@ -6,5 +8,6 @@ def CountFrequency(arr):
 if __name__ == "__main__":
 	arr = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 4, 5, 5] 
 	freq = CountFrequency(arr) 
+	# iterate dictionary named as freq to print count of each element  
 	for key, value in freq.iteritems(): 
-		print key, " -> ", value 
+		print(key, " -> ", value)
